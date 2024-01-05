703827de139cdde25a19e64f1b35ab2a40213c39
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-01-05 16:30:47 -0300

    configurando router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { Link } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-
+      <Link to="scrolls">
+        <h1>Pergaminhos Magicos</h1>
+        <img src="" alt="" />
+      </Link>
+      <Link to="weapons">
+        <h1>Armas</h1>
+        <img src="" alt="" />
+      </Link>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/scrolls",
+    element: <h1>Pergaminhos Magicos</h1>,
+  },
+  {
+    path: "/weapons",
+    element: <h1>Armas</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router} />
   </React.StrictMode>,
 )
