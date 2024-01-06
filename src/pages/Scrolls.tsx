3c52f5e4cdffae87a3c38f5c726d5503d52f9018
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-01-06 20:13:14 -0300

    finalizando filtro por nome

--- src/pages/Scrolls.tsx
+++ src/pages/Scrolls.tsx
@@ -1,17 +1,22 @@
-// import FilterByName from "../components/Filters/FilterByName";
+import FilterByName from "../components/Filters/FilterByName";
 import scrolls from "../data/scrolls.json";
 import Item from "../components/Item";
+import { useState } from "react";
 
 export default function Scrolls() {
+  const [filterName, setFilterName] = useState("");
+  // console.log(filterName);
+  
   return (
     <>
     <div>
-      {/* <FilterByName /> */}
+      <FilterByName handleChange={({ target: { value } }) => setFilterName(value)} />
     </div>
     <div className="grid grid-cols-4 gap-4">
       {
-        scrolls.map((scroll) => (
+        scrolls.filter(({name}) => name.toLowerCase().includes(filterName.toLowerCase())).map((scroll, index) => (
           <Item
+          key={index}
           name={scroll.name}
           description={scroll.description}
           image={scroll.image}
@@ -22,42 +27,7 @@ export default function Scrolls() {
           link={scroll.link} />
         ))
       }
-      {/* <Item
-      name={scrolls[0].name}
-      description={scrolls[0].description}
-      image={scrolls[0].image}
-      properties={scrolls[0].properties}
-      details={scrolls[0].details}
-      effect={scrolls[0].effect}
-      condition={scrolls[0].condition}
-      link={scrolls[0].link}
-      />
-      <Item
-      name={scrolls[1].name}
-      description={scrolls[1].description}
-      image={scrolls[1].image}
-      properties={scrolls[1].properties}
-      details={scrolls[1].details}
-      effect={scrolls[1].effect}
-      condition={scrolls[1].condition}
-      link={scrolls[1].link}
-      />
-      <Item
-      name={scrolls[2].name}
-      description={scrolls[2].description}
-      image={scrolls[2].image}
-      properties={scrolls[2].properties}
-      details={scrolls[2].details}
-      effect={scrolls[2].effect}
-      condition={scrolls[2].condition}
-      link={scrolls[2].link}
-      /> */}
     </div>
     </>
-    
-    // filterByName
-    // filterByRarity
-    // FilterByPrice, smaller than, equal to, greater than
-    // sortingr
   )
 }
